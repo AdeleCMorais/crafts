d6ca9cbd641321e52deb3ac71a838a7b5da56fb9
AdeleCMorais <adelecristine@hotmail.com>
2022-02-03 18:45:34 -0300

    adiciona api e atualiza icone linkedin

--- API/main.js
+++ API/main.js
@@ -2,7 +2,7 @@ fetch("https://www.mercadobitcoin.net/api/BTC/ticker/").then((resposta)=> {
 return resposta.json()
 
 }).then(data => {
-    document.getElementById('tabela').innerHTML = data.ticker.high
+    
     document.getElementById('mocha').innerHTML = data.ticker.last
 })
 
